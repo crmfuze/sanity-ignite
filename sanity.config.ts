216199fd5bbf9fa0afff9210b935a2c6e8d9345e
jr-blanco <blancorjustin@gmail.com>
2025-04-15 15:37:57 -0700

    config

--- sanity.config.ts
+++ sanity.config.ts
@@ -43,7 +43,7 @@ function resolveHref(documentType?: string, slug?: string): string | undefined {
 export default defineConfig({
   basePath: '/studio',
   name: 'default',
-  title: 'Sanity Ignite',
+  title: 'Ambrosia Global Studio',
   projectId: clientEnv.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: clientEnv.NEXT_PUBLIC_SANITY_DATASET,
   plugins: [
