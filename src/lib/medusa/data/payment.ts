39dc91b272a37d60afc90083cfe8bc2002d713cb
jr-blanco <blancorjustin@gmail.com>
2025-04-14 17:34:41 -0700

    added medusajs data loaders

---
+++ src/lib/medusa/data/payment.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import type { HttpTypes } from '@medusajs/types';
+
+import { sdk } from '../config';
+
+import { getAuthHeaders, getCacheOptions } from './cookies';
+
+export const listCartPaymentMethods = async (regionId: string) => {
+  const headers = {
+    ...(await getAuthHeaders()),
+  };
+
+  const next = {
+    ...(await getCacheOptions('payment_providers')),
+  };
+
+  return sdk.client
+    .fetch<HttpTypes.StorePaymentProviderListResponse>(`/store/payment-providers`, {
+      method: 'GET',
+      query: { region_id: regionId },
+      headers,
+      next,
+      cache: 'no-cache',
+    })
+    .then(({ payment_providers }) =>
+      payment_providers.sort((a, b) => {
+        return a.id > b.id ? 1 : -1;
+      }),
+    )
+    .catch(() => {
+      return null;
+    });
+};
