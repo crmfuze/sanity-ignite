0f688cbef967cb04d61b95ac73e78921be592d3f
jr-blanco <blancorjustin@gmail.com>
2025-05-06 10:10:00 -0700

    fix: order confirmation product title

--- src/components/modules/order/components/item/index.tsx
+++ src/components/modules/order/components/item/index.tsx
@@ -1,10 +1,10 @@
-import { type HttpTypes } from "@medusajs/types";
-import { Table, Text } from "@medusajs/ui";
+import { type HttpTypes } from '@medusajs/types';
+import { Table, Text } from '@medusajs/ui';
 
-import LineItemOptions from "@/components/modules/common/components/line-item-options";
-import LineItemPrice from "@/components/modules/common/components/line-item-price";
-import LineItemUnitPrice from "@/components/modules/common/components/line-item-unit-price";
-import Thumbnail from "@/components/modules/products/components/thumbnail";
+import LineItemOptions from '@/components/modules/common/components/line-item-options';
+import LineItemPrice from '@/components/modules/common/components/line-item-price';
+import LineItemUnitPrice from '@/components/modules/common/components/line-item-unit-price';
+import Thumbnail from '@/components/modules/products/components/thumbnail';
 
 type ItemProps = {
   item: HttpTypes.StoreCartLineItem | HttpTypes.StoreOrderLineItem;
@@ -21,11 +21,8 @@ const Item = ({ item, currencyCode }: ItemProps) => {
       </Table.Cell>
 
       <Table.Cell className="text-left">
-        <Text
-          className="txt-medium-plus text-ui-fg-base"
-          data-testid="product-name"
-        >
-          {item.title}
+        <Text className="txt-medium-plus text-ui-fg-base" data-testid="product-name">
+          {item.product_title}
         </Text>
         <LineItemOptions variant={item.variant} data-testid="product-variant" />
       </Table.Cell>
@@ -34,20 +31,12 @@ const Item = ({ item, currencyCode }: ItemProps) => {
         <span className="!pr-0 flex flex-col items-end h-full justify-center">
           <span className="flex gap-x-1 ">
             <Text className="text-ui-fg-muted">
-              <span data-testid="product-quantity">{item.quantity}</span>x{" "}
+              <span data-testid="product-quantity">{item.quantity}</span>x{' '}
             </Text>
-            <LineItemUnitPrice
-              item={item}
-              style="tight"
-              currencyCode={currencyCode}
-            />
+            <LineItemUnitPrice item={item} style="tight" currencyCode={currencyCode} />
           </span>
 
-          <LineItemPrice
-            item={item}
-            style="tight"
-            currencyCode={currencyCode}
-          />
+          <LineItemPrice item={item} style="tight" currencyCode={currencyCode} />
         </span>
       </Table.Cell>
     </Table.Row>
